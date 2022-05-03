d8e6685f871d14c03fb7ab1a5919cd6b167a18f1
luizhakan <uefibiosxx@gmail.com>
2022-05-03 05:42:19 -0300

    Capítulo 3 Somente uma alteração na linha 4, comentário indicando a pasta 003

--- Capítulo-003/003-metodos-array.js
+++ Capítulo-003/003-metodos-array.js
@@ -1,3 +1,4 @@
 // este arquivo vai ficar um pouco grande ksks
 let atArr = [1, 2, 3, 4];
 console.log(atArr.at(-2)); // o método "length = length" - 1 foi simplificado. aqui eu mostro o elemento do meu array de trás pra frente, ou seja, o -2 quer dizer que eu vou mostrar o penúltimo item do meu array
+// então eu decidi criar uma pasta 003
