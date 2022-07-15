251fdae66ca40d29fab11549141798d8880f0e04
luizhakan <uefibiosxx@gmail.com>
2022-07-14 21:04:39 -0300

    Of 14/07/2022 - Método array of.

---
+++ Capítulo-003/003-metodos-array/020of.js
@@ -0,0 +1,6 @@
+// cria um novo array com um número ou variável de argumentos
+let numeros = Array.of(1, 2, 3); // [ 1, 2, 3 ]
+let arrayObjeto = Array.of(
+  { nome: "Luiz", idade: 20 },
+  { nome: "Izabel", idade: 22 }
+); // [ { nome: 'Luiz', idade: 20 }, { nome: 'Izabel', idade: 22 } ]
