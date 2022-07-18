b5eb1d7922effe0fc02fe1677489aecc186ef3ff
luizhakan <uefibiosxx@gmail.com>
2022-07-17 22:17:44 -0300

    Join 17/07/2022 - Aqui no método join eu mesclo alguns outros métodos para um melhor entendimento.

---
+++ Capítulo-003/003-metodos-array/016join.js
@@ -0,0 +1,4 @@
+/*Vamos juntar uns métodos e fazer com números?*/
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const metodo2 = numeros.reverse().join(' - ').toString(); // 10 - 9 - 8 - 7 - 6 - 5 - 4 - 3 - 2 - 1
+console.log(metodo2);
